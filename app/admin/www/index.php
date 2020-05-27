fed84842f602cfea9ba5913f01eb48059bfb60f6
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-05-27 15:26:35 +0500

    dir permissions test

--- app/admin/www/index.php
+++ app/admin/www/index.php
@@ -8,9 +8,30 @@ var_dump("Admin");
 $kernel = \App\Common\Kernel::Bootstrap();
 var_dump($kernel->dirs()->root()->path());
 var_dump($kernel->dirs()->root()->permissions());
-var_dump($kernel->dirs()->storage()->path());
-var_dump($kernel->dirs()->storage()->permissions());
-var_dump($kernel->dirs()->log()->path());
-var_dump($kernel->dirs()->log()->permissions());
-var_dump($kernel->dirs()->config()->path());
-var_dump($kernel->dirs()->config()->permissions());
+
+try {
+    var_dump($kernel->dirs()->storage()->path());
+    var_dump($kernel->dirs()->storage()->permissions());
+} catch (Exception $e) {
+    var_dump(get_class($e));
+    var_dump($e->getMessage());
+    var_dump($e->getTraceAsString());
+}
+
+try {
+    var_dump($kernel->dirs()->log()->path());
+    var_dump($kernel->dirs()->log()->permissions());
+} catch (Exception $e) {
+    var_dump(get_class($e));
+    var_dump($e->getMessage());
+    var_dump($e->getTraceAsString());
+}
+
+try {
+    var_dump($kernel->dirs()->config()->path());
+    var_dump($kernel->dirs()->config()->permissions());
+} catch (Exception $e) {
+    var_dump(get_class($e));
+    var_dump($e->getMessage());
+    var_dump($e->getTraceAsString());
+}
