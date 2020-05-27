6ba385c13158017155631c14926f30538f27ee19
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-05-27 14:51:21 +0500

    test index

--- app/admin/www/index.php
+++ app/admin/www/index.php
@@ -2,3 +2,13 @@
 declare(strict_types=1);
 
 var_dump("Admin");
+
+$kernel = \App\Common\Kernel::Bootstrap();
+var_dump($kernel->dirs()->root()->path());
+var_dump($kernel->dirs()->root()->permissions());
+var_dump($kernel->dirs()->storage()->path());
+var_dump($kernel->dirs()->storage()->permissions());
+var_dump($kernel->dirs()->log()->path());
+var_dump($kernel->dirs()->log()->permissions());
+var_dump($kernel->dirs()->config()->path());
+var_dump($kernel->dirs()->config()->permissions());
