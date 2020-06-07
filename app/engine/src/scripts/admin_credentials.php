57f559f51654d1ea8213c1c11dc176154e7d2ef5
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-07 18:34:10 +0500

    correct class path

--- app/engine/src/scripts/admin_credentials.php
+++ app/engine/src/scripts/admin_credentials.php
@@ -55,10 +55,6 @@ class admin_credentials extends AbstractCLIScript
 
         // DB
         $db = $this->app->db()->primary();
-        $adminsTable = 'App\Common\Database\Primary\Administrators';
-        var_dump(OOP::isValidClassName($adminsTable));
-        var_dump(class_exists($adminsTable));
-        var_dump(OOP::isValidClass($adminsTable));
         Schema::Bind($db, 'App\Common\Database\Primary\Administrators');
 
         // Create Administrator Object
