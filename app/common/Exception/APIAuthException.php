efd400bc35920c4b09488dfddb0b52e9a44ed389
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-09 19:21:22 +0500

    API_Exception

---
+++ app/common/Exception/APIAuthException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\Exception;
+
+/**
+ * Class APIAuthException
+ * @package App\Common\Exception
+ */
+class APIAuthException extends API_Exception
+{
+}
---
+++ app/common/Exception/API_Exception.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\Exception;
+
+/**
+ * Class API_Exception
+ * @package App\Common\Exception
+ */
+class API_Exception extends AppControllerException
+{
+}
