52d492d82409ff40e336b49e52b3789cd0e06f6a
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-09 21:34:46 +0500

    abstract HTTP app kernels

--- app/admin/src/AppAdmin.php
+++ app/admin/src/AppAdmin.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace App\Admin;
 
 use App\Common\Kernel;
+use Comely\Knit\Knit;
+use Comely\Sessions\Sessions;
+use Comely\Sessions\Storage\SessionDirectory;
 
 /**
  * Class AppAdmin
@@ -11,4 +14,49 @@ use App\Common\Kernel;
  */
 class AppAdmin extends Kernel\AbstractHttpApp
 {
+    /** @var Sessions */
+    private Sessions $sess;
+    /** @var Knit */
+    private Knit $knit;
+
+    /**
+     * AppAdmin constructor.
+     * @throws \App\Common\Exception\AppConfigException
+     * @throws \App\Common\Exception\AppDirException
+     * @throws \Comely\Filesystem\Exception\PathException
+     * @throws \Comely\Filesystem\Exception\PathNotExistException
+     * @throws \Comely\Filesystem\Exception\PathOpException
+     * @throws \Comely\Sessions\Exception\StorageException
+     * @throws \Comely\Yaml\Exception\ParserException
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Sessions
+        $this->sess = new Sessions(new SessionDirectory($this->dirs()->sessions()));
+
+        // Knit
+        $this->knit = new Knit();
+        $this->knit->dirs()->compiler($this->dirs()->knit())
+            ->cache($this->dirs()->knit()->dir("cache"));
+
+        $this->knit->modifiers()->registerDefaultModifiers();
+    }
+
+    /**
+     * @return Sessions
+     */
+    public function sessions(): Sessions
+    {
+        return $this->sess;
+    }
+
+    /**
+     * @return Knit
+     */
+    public function knit(): Knit
+    {
+        return $this->knit;
+    }
 }
--- app/common/Kernel/AbstractHttpApp.php
+++ app/common/Kernel/AbstractHttpApp.php
@@ -5,9 +5,6 @@ namespace App\Common\Kernel;
 
 use App\Common\Kernel;
 use Comely\Http\Router;
-use Comely\Knit\Knit;
-use Comely\Sessions\Sessions;
-use Comely\Sessions\Storage\SessionDirectory;
 
 /**
  * Class AbstractHttpApp
@@ -17,35 +14,17 @@ abstract class AbstractHttpApp extends Kernel
 {
     /** @var Kernel\Http */
     private Kernel\Http $http;
-    /** @var Sessions */
-    private Sessions $sess;
-    /** @var Knit */
-    private Knit $knit;
 
     /**
      * AbstractHttpApp constructor.
      * @throws \App\Common\Exception\AppConfigException
      * @throws \App\Common\Exception\AppDirException
-     * @throws \Comely\Filesystem\Exception\PathException
-     * @throws \Comely\Filesystem\Exception\PathNotExistException
-     * @throws \Comely\Filesystem\Exception\PathOpException
-     * @throws \Comely\Sessions\Exception\StorageException
      * @throws \Comely\Yaml\Exception\ParserException
      */
     protected function __construct()
     {
         parent::__construct();
         $this->http = new Kernel\Http($this);
-
-        // Sessions
-        $this->sess = new Sessions(new SessionDirectory($this->dirs()->sessions()));
-
-        // Knit
-        $this->knit = new Knit();
-        $this->knit->dirs()->compiler($this->dirs()->knit())
-            ->cache($this->dirs()->knit()->dir("cache"));
-
-        $this->knit->modifiers()->registerDefaultModifiers();
     }
 
     /**
@@ -62,21 +41,5 @@ abstract class AbstractHttpApp extends Kernel
     public function http(): Kernel\Http
     {
         return $this->http;
-    }
-
-    /**
-     * @return Sessions
-     */
-    public function sessions(): Sessions
-    {
-        return $this->sess;
-    }
-
-    /**
-     * @return Knit
-     */
-    public function knit(): Knit
-    {
-        return $this->knit;
     }
 }
