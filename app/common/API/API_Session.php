7ddb178176dd5235d8ae86dda6647bcd8f9af654
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-09 19:21:36 +0500

    API_Session

---
+++ app/common/API/API_Session.php
@@ -0,0 +1,273 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\API;
+
+use App\Common\Database\AbstractAppModel;
+use App\Common\Database\API\Sessions;
+use App\Common\Database\Primary\Users;
+use App\Common\Exception\APIAuthException;
+use App\Common\Exception\AppException;
+use App\Common\Users\User;
+use Comely\Database\Exception\DbQueryException;
+use Comely\DataTypes\Buffer\Base16;
+use Comely\DataTypes\Buffer\Binary;
+use Comely\Utils\Time\Time;
+
+/**
+ * Class API_Session
+ * @package App\Common\API
+ */
+class API_Session extends AbstractAppModel
+{
+    public const TABLE = Sessions::NAME;
+    public const SERIALIZABLE = false;
+
+    /** @var int */
+    public int $id;
+    /** @var int */
+    public int $archived;
+    /** @var string */
+    public string $type;
+    /** @var string */
+    public string $ipAddress;
+    /** @var null|int */
+    public ?int $authUserId = null;
+    /** @var null|int */
+    public ?int $authSessionOtp = null;
+    /** @var null|int */
+    public ?int $recaptchaLast = null;
+    /** @var int */
+    public int $issuedOn;
+    /** @var int */
+    public int $lastUsedOn;
+
+    /** @var APISessBaggage|null */
+    private ?APISessBaggage $_baggage = null;
+    /** @var null|User */
+    private ?User $_authUser = null;
+    /** @var Base16|null */
+    private ?Base16 $_tokenHex = null;
+    /** @var int|null */
+    public ?int $_lastUsedOn = null;
+    /** @var bool|null */
+    public ?bool $_checksumValidated = null;
+
+    /**
+     * @return Base16
+     * @throws AppException
+     */
+    public function token(): Base16
+    {
+        if (!$this->_tokenHex) {
+            $token = $this->private("token");
+            if (!$token) {
+                throw new AppException('API session token undefined');
+            }
+
+            $this->_tokenHex = (new Binary($token))->base16()->readOnly(true);
+        }
+
+        return $this->_tokenHex;
+    }
+
+    /**
+     * @return Binary
+     * @throws \App\Common\Exception\AppConfigException
+     * @throws \Comely\Utils\Security\Exception\CipherException
+     */
+    public function checksum(): Binary
+    {
+        $token = $this->private("token");
+        if (!is_string($token) || !$token) {
+            throw new \UnexpectedValueException('API session has no token ID');
+        }
+
+        $raw = sprintf(
+            '%d:%s:%s:%s:%d:%d',
+            $this->id,
+            $this->type,
+            $token,
+            $this->ipAddress,
+            $this->authUserId ?? 0,
+            $this->authSessionOtp === 1 ? 1 : 0
+        );
+
+        return $this->app->ciphers()->users()->pbkdf2("sha1", $raw, 1000);
+    }
+
+    /**
+     * @throws AppException
+     * @throws \App\Common\Exception\AppConfigException
+     * @throws \Comely\Utils\Security\Exception\CipherException
+     */
+    public function validate(): void
+    {
+        $token = $this->private("token");
+        if (!is_string($token) || !$token) {
+            throw new \UnexpectedValueException('API session has no token ID');
+        }
+
+        $checksum = $this->private("checksum") ?? null;
+        if ($this->checksum()->raw() !== $checksum) {
+            throw new AppException('SESSION_CHECKSUM_FAIL');
+        }
+
+        $this->_checksumValidated = true;
+    }
+
+    /**
+     * @param bool $forceRecheck
+     * @return User
+     * @throws APIAuthException
+     * @throws AppException
+     */
+    public function authenticate(bool $forceRecheck = false): User
+    {
+        if ($this->_authUser && !$forceRecheck) {
+            return $this->_authUser;
+        }
+
+        if (!$this->authUserId) {
+            throw new APIAuthException('AUTH_NOT_LOGGED_IN');
+        }
+
+        try {
+            try {
+                $user = Users::get($this->authUserId);
+                $user->validate();
+                $user->credentials();
+            } catch (AppException $e) {
+                $this->app->errors()->triggerIfDebug($e, E_USER_WARNING);
+                throw new APIAuthException('AUTH_USER_RETRIEVE_ERROR');
+            }
+
+            // Is current session
+            if ($this->private("token") !== $user->private("authToken")) {
+                throw new APIAuthException('AUTH_TOKEN_MISMATCH');
+            }
+
+            // Check status
+            if (!in_array($user->status, ["active", "frozen"])) {
+                throw new APIAuthException('AUTH_USER_DISABLED');
+            }
+
+            // Check Timeout
+            if (is_int($this->_lastUsedOn) && Time::difference($this->_lastUsedOn) >= 1800) {
+                try {
+                    $user->log('session-timeout', null, null, null, ["auth"]);
+                } catch (\Exception $e) {
+                    $this->app->errors()->triggerIfDebug($e);
+                    throw new AppException('Failed to create user session timeout log');
+                }
+
+                throw new APIAuthException('AUTH_USER_TIMEOUT');
+            }
+
+            // Set User object as Auth
+            $this->_authUser = $user;
+            return $this->_authUser;
+        } catch (APIAuthException $e) {
+            // Logout
+            try {
+                $this->logout();
+            } catch (AppException $e) {
+                trigger_error($e->getMessage(), E_USER_WARNING);
+            }
+
+            throw $e;
+        }
+    }
+
+    /**
+     * @param User $user
+     * @param bool $archivePrevSessions
+     * @return bool
+     * @throws AppException
+     */
+    public function loginAs(User $user, bool $archivePrevSessions = true): bool
+    {
+        try {
+            $this->authUserId = $user->id;
+            $this->set("checksum", $this->checksum()->raw());
+            $this->query()->where("id", $this->id)->update(function () {
+                throw new AppException('Failed to authenticate current session');
+            });
+
+            $user->deleteCached();
+
+            // Archive all previous tokens
+            if ($archivePrevSessions) {
+                $apiDb = $this->app->db()->apiLogs();
+
+                try {
+                    $apiDb->exec(
+                        sprintf('UPDATE `%s` SET `archived`=1 WHERE `auth_user_id`=? AND `id`<?', Sessions::NAME),
+                        [$user->id, $this->id]
+                    );
+                } catch (DbQueryException $e) {
+                    throw new AppException('Failed to archive previous sessions from this user');
+                }
+            }
+
+            return true;
+        } catch (\Exception $e) {
+            $this->app->errors()->triggerIfDebug($e, E_USER_WARNING);
+            throw new AppException('API session login logic fail');
+        }
+    }
+
+    /**
+     * @throws AppException
+     */
+    public function logout(): void
+    {
+        try {
+            $this->authUserId = null;
+            $this->set("checksum", $this->checksum()->raw());
+            $this->query()->where("id", $this->id)->update(function () {
+                throw new AppException('Failed to un-authenticate current session');
+            });
+        } catch (\Exception $e) {
+            $this->app->errors()->triggerIfDebug($e, E_USER_WARNING);
+            throw new AppException('API session logout fail');
+        }
+    }
+
+    /**
+     * @return bool
+     * @throws AppException
+     */
+    public function markAuthSessionOTP(): bool
+    {
+        if (!$this->authUserId) {
+            throw new AppException('Cannot use method without setting authUserId');
+        }
+
+        try {
+            $this->authSessionOtp = 1;
+            $this->set("checksum", $this->checksum()->raw());
+            $this->query()->where("id", $this->id)->update(function () {
+                throw new AppException('Failed to OTP authenticate current session');
+            });
+
+            return true;
+        } catch (\Exception $e) {
+            $this->app->errors()->triggerIfDebug($e, E_USER_WARNING);
+            throw new AppException('API session OTP authenticate logic fail');
+        }
+    }
+
+    /**
+     * @return APISessBaggage
+     */
+    public function baggage(): APISessBaggage
+    {
+        if ($this->_baggage) {
+            return $this->_baggage;
+        }
+
+        $this->_baggage = new APISessBaggage($this);
+        return $this->_baggage;
+    }
+}
---
+++ app/common/Database/API/Sessions.php
@@ -0,0 +1,203 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\Database\API;
+
+use App\Common\API\API_Session;
+use App\Common\Database\AbstractAppTable;
+use App\Common\Database\Primary\Users;
+use App\Common\Exception\API_Exception;
+use App\Common\Exception\AppException;
+use App\Common\Kernel;
+use App\Common\Kernel\Databases;
+use App\Common\Validator;
+use Comely\Database\Exception\ORM_ModelNotFoundException;
+use Comely\Database\Schema\Table\Columns;
+use Comely\Database\Schema\Table\Constraints;
+use Comely\DataTypes\Buffer\Binary;
+use Comely\Utils\Security\PRNG;
+
+/**
+ * Class Sessions
+ * @package App\Common\Database\API
+ */
+class Sessions extends AbstractAppTable
+{
+    public const TOKEN_TYPES = ["web", "desktop", "mobile"];
+    public const REQUEST_TOKEN_TIMEOUT_PER_IP = 60;
+
+    public const NAME = 'api_sess';
+    public const MODEL = 'App\API\API_Session';
+
+    /**
+     * @param Columns $cols
+     * @param Constraints $constraints
+     * @throws AppException
+     */
+    public function structure(Columns $cols, Constraints $constraints): void
+    {
+        $cols->defaults("ascii", "ascii_general_ci");
+
+        $cols->int("id")->bytes(8)->unSigned()->autoIncrement();
+        $cols->int("archived")->bytes(1)->unSigned()->default(0);
+        $cols->enum("type")->options("web", "desktop", "mobile")->default("web");
+        $cols->binary("checksum")->fixed(20);
+        $cols->binary("token")->fixed(32)->unique();
+        $cols->string("ip_address")->length(45);
+        $cols->int("auth_user_id")->unSigned()->nullable();
+        $cols->int("auth_session_otp")->bytes(1)->unSigned()->nullable();
+        $cols->int("recaptcha_last")->bytes(4)->unSigned()->nullable();
+        $cols->int("issued_on")->bytes(4)->unSigned();
+        $cols->int("last_used_on")->bytes(4)->unSigned();
+        $cols->primaryKey("id");
+
+        // Primary Database Name
+        $primaryDBName = $this->app->db()->getDbName(Databases::PRIMARY);
+        if (!$primaryDBName) {
+            throw new AppException('Failed to retrieve PRIMARY database name from config');
+        }
+
+        $constraints->foreignKey("auth_user_id")->database($primaryDBName)->table(Users::NAME, "id");
+    }
+
+    /**
+     * @param Binary|null $token
+     * @param string|null $ip
+     * @return API_Session
+     * @throws API_Exception
+     */
+    public static function getSession(?Binary $token = null, ?string $ip = null): API_Session
+    {
+        $k = Kernel::getInstance();
+
+        if ($token) {
+            if ($token->len() !== 32) {
+                throw new \InvalidArgumentException('Invalid token length; Expecting 32 bytes');
+            }
+
+            $tokenHex = $token->base16()->hexits(false);
+            $memoryId = sprintf('api_sess_%s', $tokenHex);
+            $queryCol = "token";
+            $queryVal = $tokenHex;
+        } elseif ($ip) {
+            if (!Validator::isValidIP($ip, true)) {
+                throw new \InvalidArgumentException('Invalid IP address');
+            }
+
+            $memoryId = sprintf('api_sess_%s', md5($ip));
+            $queryCol = "ip_address";
+            $queryVal = $ip;
+        } else {
+            throw new \InvalidArgumentException('No token or IP supplied as argument');
+        }
+
+        try {
+            return $k->memory()->query($memoryId, self::MODEL)
+                ->fetch(function () use ($queryCol, $queryVal) {
+                    return self::Find()->col($queryCol, $queryVal)->limit(1)->first();
+                });
+        } catch (\Exception $e) {
+            if ($k->isDebug()) {
+                if (!$e instanceof ORM_ModelNotFoundException) {
+                    throw new API_Exception('SESSION_NOT_FOUND');
+                }
+            }
+
+            throw new API_Exception('SESSION_RETRIEVE_ERROR');
+        }
+    }
+
+    /**
+     * @param string $ip
+     * @param string $type
+     * @return Binary
+     * @throws API_Exception
+     * @throws AppException
+     * @throws \Comely\Database\Exception\DatabaseException
+     */
+    public static function issueToken(string $ip, string $type = "web"): Binary
+    {
+        $k = Kernel::getInstance();
+        $db = $k->db()->apiLogs();
+
+        if ($db->inTransaction()) {
+            throw new AppException('API database already in transaction mode');
+        }
+
+        if (!in_array($type, self::TOKEN_TYPES)) {
+            throw new \InvalidArgumentException('Invalid session token type');
+        }
+
+        try {
+            $timeStamp = time();
+            $secureEntropy = PRNG::randomBytes(32);
+
+            $token = new API_Session();
+            $token->id = 0;
+            $token->archived = 0;
+            $token->set("checksum", "tba");
+            $token->type = $type;
+            $token->set("token", $secureEntropy->raw());
+            $token->ipAddress = $ip;
+            $token->authUserId = null;
+            $token->recaptchaLast = null;
+            $token->issuedOn = $timeStamp;
+            $token->lastUsedOn = $timeStamp;
+
+            $db->beginTransaction();
+
+            // Check token is unique
+            $tokenUniqueQuery = $db->query()->table(self::NAME)
+                ->where('`token`=?', [$secureEntropy->raw()])
+                ->limit(1)
+                ->fetch();
+
+            if ($tokenUniqueQuery->count()) {
+                // Non-unique token, repeat
+                return self::issueToken($ip, $type);
+            }
+
+            // Requesting Tokens too often?
+            $timeOutPerIP = self::REQUEST_TOKEN_TIMEOUT_PER_IP;
+            if ($timeOutPerIP) {
+                $checkSince = $timeStamp - $timeOutPerIP;
+                $lastTokenForIp = $db->query()->table(self::NAME)
+                    ->where('`ip_address`=? AND `issued_on`>=?', [$ip, $checkSince])
+                    ->limit(1)
+                    ->fetch();
+
+                if ($lastTokenForIp->count()) {
+                    throw new API_Exception('SESSION_CREATE_TIMEOUT');
+                }
+            }
+
+            $insert = $token->query()->insert();
+            if (!$insert->isSuccess(true) || !is_int($db->lastInsertId())) {
+                throw new AppException('Failed to insert API session token in DB');
+            }
+
+            $token->id = $db->lastInsertId();
+            $token->set("checksum", $token->checksum()->raw());
+            $token->query()->where('id', $token->id)->update(function () {
+                throw new AppException('Failed to finalise API session row');
+            });
+
+            $db->commit();
+        } catch (API_Exception $e) {
+            if ($db->inTransaction()) {
+                $db->rollBack();
+            }
+
+            throw $e;
+        } catch (\Exception $e) {
+            if ($db->inTransaction()) {
+                $db->rollBack();
+            }
+
+            $k->errors()->triggerIfDebug($e, E_USER_WARNING);
+            throw new API_Exception('SESSION_CREATE_ERROR');
+        }
+
+        return $secureEntropy->readOnly(true);
+    }
+}
