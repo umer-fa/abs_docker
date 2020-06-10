38897ebb7563e12909eed1bf34174a5b119ca379
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-10 19:40:30 +0500

    updates

--- app/admin/src/Controllers/Api/Access.php
+++ app/admin/src/Controllers/Api/Access.php
@@ -5,6 +5,7 @@ namespace App\Admin\Controllers\Api;
 
 use App\Admin\Controllers\AbstractAdminController;
 use App\Common\Config\APIServerAccess;
+use App\Common\Exception\AppControllerException;
 use App\Common\Exception\AppException;
 use Comely\Utils\Time\Time;
 use Comely\Utils\Time\TimeUnits;
@@ -31,9 +32,71 @@ class Access extends AbstractAdminController
         }
     }
 
+    /**
+     * @throws AppControllerException
+     * @throws AppException
+     * @throws \App\Common\Exception\XSRF_Exception
+     * @throws \Comely\Database\Exception\DatabaseException
+     * @throws \ReflectionException
+     */
     public function post(): void
     {
+        $this->verifyXSRF();
+        $this->totpSessionCheck();
 
+        if (!$this->authAdmin->privileges()->root()) {
+            if (!$this->authAdmin->privileges()->editConfig) {
+                throw new AppControllerException('You are not authorized to update configuration');
+            }
+        }
+
+        $changes = 0;
+
+        $reflect = new \ReflectionClass($this->apiServerAccess);
+        $triggers = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC);
+        foreach ($triggers as $trigger) {
+            $propId = $trigger->name;
+            $current = $this->apiServerAccess->$propId;
+            $this->apiServerAccess->$propId = $this->input()->get($propId) === "on";
+            if ($this->apiServerAccess->$propId !== $current) {
+                $changes++;
+            }
+        }
+
+        if (!$changes) {
+            throw new AppControllerException('There are no changes to be saved!');
+        }
+
+        $db = $this->app->db()->primary();
+        try {
+            $db->beginTransaction();
+
+            // Save configuration in DB
+            $this->apiServerAccess->save();
+
+            // Admin Log
+            $this->authAdmin->log('API Accessibility Setup Updated', null, null, ["config"]);
+
+            $db->commit();
+        } catch (AppException $e) {
+            $db->rollBack();
+            throw $e;
+        } catch (\Exception $e) {
+            $db->rollBack();
+            $this->app->errors()->triggerIfDebug($e, E_USER_WARNING);
+            throw new AppException('Failed to update API accessibility');
+        }
+
+        // Delete Cached
+        try {
+            $this->app->cache()->delete(APIServerAccess::CACHE_KEY);
+        } catch (\Exception $e) {
+        }
+
+        $this->response()->set("status", true);
+        $this->messages()->success('API accessibility configuration updated!');
+        $this->messages()->info('Refreshing page...');
+        $this->response()->set("refresh", true);
     }
 
     /**
--- app/common/Config/APIServerAccess.php
+++ app/common/Config/APIServerAccess.php
@@ -9,6 +9,11 @@ namespace App\Common\Config;
  */
 class APIServerAccess extends AbstractConfigObj
 {
+    public const DB_KEY = "app.apiServerAccess";
+    public const CACHE_KEY = "app.apiServerAccess";
+    public const CACHE_TTL = 86400;
+    public const IS_ENCRYPTED = false;
+
     /** @var bool */
     public bool $globalStatus = false;
     /** @var bool */
