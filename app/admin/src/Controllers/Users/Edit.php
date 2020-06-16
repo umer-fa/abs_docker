7f67d931600e2b3854dfa01da5252b4e421ad331
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-16 22:53:30 +0500

    edit user incomplet

--- app/admin/src/Controllers/Users/Edit.php
+++ app/admin/src/Controllers/Users/Edit.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 namespace App\Admin\Controllers\Users;
 
 use App\Admin\Controllers\AbstractAdminController;
+use App\Common\Database\Primary\Countries;
+use App\Common\Database\Primary\Users;
+use App\Common\Exception\AppControllerException;
+use App\Common\Exception\AppException;
 use App\Common\Users\User;
+use App\Common\Validator;
 use Comely\Database\Schema;
 
 /**
@@ -13,13 +18,58 @@ use Comely\Database\Schema;
  */
 class Edit extends AbstractAdminController
 {
+    /** @var User */
     private User $user;
 
+    /**
+     * @throws \App\Common\Exception\AppConfigException
+     * @throws \Comely\Database\Exception\DbConnectionException
+     */
     public function adminCallback(): void
     {
         $db = $this->app->db()->primary();
+        Schema::Bind($db, 'App\Common\Database\Primary\Countries');
         Schema::Bind($db, 'App\Common\Database\Primary\Users');
-        $queryUserId = $this->request()->url()->query();
-        var_dump($queryUserId);
+
+        try {
+            $queryUserId = explode("=", strval($this->request()->url()->query()))[0];
+            $userId = Validator::UInt($this->input()->get("userId") ?? $queryUserId ?? null);
+            if (!$userId) {
+                throw new AppControllerException('No user selected');
+            }
+
+            $this->user = Users::get($userId);
+        } catch (\Exception $e) {
+            if ($e instanceof AppException) {
+                $this->flash()->danger($e->getMessage());
+            } else {
+                $this->app->errors()->trigger($e, E_USER_WARNING);
+            }
+
+            $this->redirect($this->authRoot . "users/search");
+            exit;
+        }
+
+        try {
+            $this->user->validate();
+        } catch (AppException $e) {
+        }
+    }
+
+    /**
+     * @throws \Comely\Knit\Exception\KnitException
+     * @throws \Comely\Knit\Exception\TemplateException
+     */
+    public function get(): void
+    {
+        $this->page()->title(sprintf('User [#%d] Overview', $this->user->id))->index(1100, 10)
+            ->prop("icon", "mdi mdi-account-edit-outline");
+
+        $this->breadcrumbs("Users Control", null, "ion ion-ios-people");
+
+        $template = $this->template("users/edit.knit")
+            ->assign("user", $this->user)
+            ->assign("countries", Countries::List());
+        $this->body($template);
     }
 }
