0fceae49589dfeef0ad8f93c0cc9fbcc2420d059
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-22 20:53:43 +0500

    static codes

--- app/common/Exception/API_Exception.php
+++ app/common/Exception/API_Exception.php
@@ -9,4 +9,27 @@ namespace App\Common\Exception;
  */
 class API_Exception extends AppControllerException
 {
+    /**
+     * @return static
+     */
+    public static function InternalError(): self
+    {
+        return new self('INTERNAL_ERROR');
+    }
+
+    /**
+     * @return static
+     */
+    public static function DatabaseQueryError(): self
+    {
+        return new self('DB_QUERY_ERROR');
+    }
+
+    /**
+     * @return static
+     */
+    public static function ControllerDisabled(): self
+    {
+        return new self('API_CONTROLLER_DISABLED');
+    }
 }
