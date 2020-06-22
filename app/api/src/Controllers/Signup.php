9bb53e3b0f369b1a877c82a8aada2dc3b86b5d21
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-22 21:23:34 +0500

    signup endpoint

---
+++ app/api/src/Controllers/Signup.php
@@ -0,0 +1,275 @@
+<?php
+declare(strict_types=1);
+
+namespace App\API\Controllers;
+
+use App\Common\Config\ProgramConfig;
+use App\Common\Database\Primary\Users;
+use App\Common\Exception\API_Exception;
+use App\Common\Exception\AppControllerException;
+use App\Common\Exception\AppException;
+use App\Common\Packages\ReCaptcha\ReCaptcha;
+use App\Common\Users\Credentials;
+use App\Common\Users\Params;
+use App\Common\Users\User;
+use App\Common\Validator;
+use Comely\DataTypes\Integers;
+use Comely\Utils\Security\Passwords;
+
+/**
+ * Class Signup
+ * @package App\API\Controllers
+ */
+class Signup extends AbstractSessionAPIController
+{
+    /**
+     * @throws API_Exception
+     */
+    public function sessionAPICallback(): void
+    {
+        if (!$this->apiAccess->signUp) {
+            throw API_Exception::ControllerDisabled();
+        }
+
+        if ($this->apiSession->authUserId) {
+            throw new API_Exception('ALREADY_LOGGED_IN');
+        }
+    }
+
+    /**
+     * @throws API_Exception
+     * @throws AppException
+     * @throws \Comely\Database\Exception\DatabaseException
+     */
+    public function post(): void
+    {
+        $db = $this->app->db()->primary();
+
+        // ReCaptcha Validation
+        if ($this->isReCaptchaRequired()) {
+            try {
+                $reCaptchaRes = $this->input()->get("reCaptchaRes");
+                if (!$reCaptchaRes || !is_string($reCaptchaRes)) {
+                    throw new API_Exception('RECAPTCHA_REQ');
+                }
+
+                $programConfig = ProgramConfig::getInstance();
+                $reCaptchaSecret = $programConfig->reCaptchaPrv;
+                if (!$reCaptchaSecret || !is_string($reCaptchaSecret)) {
+                    throw new AppException('ReCaptcha secret was not available');
+                }
+
+                try {
+                    ReCaptcha::Verify($reCaptchaSecret, $reCaptchaRes, $this->ipAddress);
+                } catch (\Exception $e) {
+                    throw new API_Exception('RECAPTCHA_FAILED');
+                }
+            } catch (API_Exception $e) {
+                $e->setParam("reCaptchaRes");
+                throw $e;
+            }
+        }
+
+        // First name
+        try {
+            $firstName = trim(strval($this->input()->get("firstName")));
+            if (!$firstName) {
+                throw new API_Exception('FIRST_NAME_REQ');
+            } elseif (!Integers::Range(strlen($firstName), 3, 16)) {
+                throw new API_Exception('FIRST_NAME_LEN');
+            } elseif (!preg_match('/^[a-z]+(\s[a-z]+)*$/i', $firstName)) {
+                throw new API_Exception('FIRST_NAME_INVALID');
+            }
+        } catch (AppException $e) {
+            $e->setParam("firstName");
+            throw $e;
+        }
+
+        // Last name
+        try {
+            $lastName = trim(strval($this->input()->get("lastName")));
+            if (!$lastName) {
+                throw new API_Exception('LAST_NAME_REQ');
+            } elseif (!Integers::Range(strlen($lastName), 3, 16)) {
+                throw new API_Exception('LAST_NAME_LEN');
+            } elseif (!preg_match('/^[a-z]+(\s[a-z]+)*$/i', $lastName)) {
+                throw new API_Exception('LAST_NAME_INVALID');
+            }
+        } catch (AppException $e) {
+            $e->setParam("lastName");
+            throw $e;
+        }
+
+        // E-mail Address
+        try {
+            $email = trim(strval($this->input()->get("email")));
+            if (!$email) {
+                throw new API_Exception('EMAIL_ADDR_REQ');
+            } elseif (!Validator::isValidEmailAddress($email)) {
+                throw new API_Exception('EMAIL_ADDR_INVALID');
+            } elseif (strlen($email) > 64) {
+                throw new API_Exception('EMAIL_ADDR_LEN');
+            }
+
+            // Duplicate check
+            $dup = $db->query()->table(Users::NAME)
+                ->where('`email`=?', [$email])
+                ->fetch();
+            if ($dup->count()) {
+                throw new API_Exception('EMAIL_ADDR_DUP');
+            }
+        } catch (AppException $e) {
+            $e->setParam("email");
+            throw $e;
+        }
+
+        // Country
+        try {
+            $country = trim(strval($this->input()->get("country")));
+            if (strlen($country) !== 3) {
+                throw new API_Exception('COUNTRY_INVALID');
+            }
+
+            try {
+                $country = \App\Common\Database\Primary\Countries::get($country);
+            } catch (AppException $e) {
+                throw new API_Exception('COUNTRY_INVALID');
+            }
+
+            if ($country->status !== 1) {
+                throw new API_Exception('COUNTRY_INVALID');
+            }
+        } catch (API_Exception $e) {
+            $e->setParam("country");
+            throw $e;
+        }
+
+        // Username
+        try {
+            $username = trim(strval($this->input()->get("username")));
+            $usernameLen = strlen($username);
+            if ($usernameLen <= 4) {
+                throw new API_Exception('USERNAME_LEN_MIN');
+            } elseif ($usernameLen >= 20) {
+                throw new API_Exception('USERNAME_LEN_MAX');
+            } elseif (!Validator::isValidUsername($username)) {
+                throw new API_Exception('USERNAME_INVALID');
+            }
+
+            // Duplicate Check
+            $dup = $db->query()->table(Users::NAME)
+                ->where('`username`=?', [$username])
+                ->fetch();
+            if ($dup->count()) {
+                throw new API_Exception('USERNAME_DUP');
+            }
+        } catch (AppException $e) {
+            $e->setParam("username");
+            throw $e;
+        }
+
+        // Password
+        try {
+            $password = trim(strval($this->input()->get("password")));
+            $passwordLen = strlen($password);
+            if (!$password) {
+                throw new API_Exception('PASSWORD_REQ');
+            } elseif ($passwordLen <= 5) {
+                throw new API_Exception('PASSWORD_LEN_MIN');
+            } elseif ($passwordLen > 32) {
+                throw new API_Exception('PASSWORD_LEN_MAX');
+            } elseif (Passwords::Strength($password) < 4) {
+                throw new API_Exception('PASSWORD_WEAK');
+            }
+        } catch (AppException $e) {
+            $e->setParam("password");
+            throw $e;
+        }
+
+        // Confirm Password
+        try {
+            $confirmPassword = trim(strval($this->input()->get("confirmPassword")));
+            if ($password !== $confirmPassword) {
+                throw new API_Exception('PASSWORD_CONFIRM_MATCH');
+            }
+        } catch (API_Exception $e) {
+            $e->setParam("confirmPassword");
+            throw $e;
+        }
+
+        // Terms and conditions
+        try {
+            $terms = trim(strval($this->input()->get("terms")));
+            if (!Validator::getBool($terms)) {
+                throw new API_Exception('TERMS_UNCHECKED');
+            }
+        } catch (API_Exception $e) {
+            $e->setParam("terms");
+            throw $e;
+        }
+
+        // Insert User?
+        try {
+            $db->beginTransaction();
+
+            $user = new User();
+            $user->id = 0;
+            $user->set("checksum", "tba");
+            $user->referrer = null;
+            $user->status = "active";
+            $user->firstName = $firstName;
+            $user->lastName = $lastName;
+            $user->username = $username;
+            $user->email = $email;
+            $user->isEmailVerified = 0;
+            $user->country = $country->code;
+            $user->phoneSms = null;
+            $user->set("credentials", "tba");
+            $user->set("params", "tba");
+            $user->joinStamp = time();
+            $user->timeStamp = time();
+
+            $user->query()->insert(function () {
+                throw new AppControllerException('Failed to insert user row');
+            });
+
+            $user->id = Validator::UInt($db->lastInsertId()) ?? 0;
+            if (!$user->id) {
+                throw new AppControllerException('Invalid user last insert ID');
+            }
+
+            // Credentials & Params
+            $credentials = new Credentials($user);
+            $credentials->hashPassword($password);
+            $params = new Params($user);
+
+            $userCipher = $user->cipher();
+
+            $user->set("checksum", $user->checksum()->raw());
+            $user->set("credentials", $userCipher->encrypt(clone $credentials)->raw());
+            $user->set("params", $userCipher->encrypt(clone $params)->raw());
+
+            // Save Changes
+            $user->query()->where('id', $user->id)->update(function () {
+                throw new AppControllerException('Failed to finalise user row');
+            });
+
+            $user->log("signup", null, null, null, ["signup"]);
+
+            // Todo: Queue email notification
+
+            $db->commit();
+        } catch (\Exception $e) {
+            $db->rollBack();
+            $this->app->errors()->triggerIfDebug($e instanceof AppException ? $e->getMessage() : $e, E_USER_WARNING);
+            throw API_Exception::InternalError();
+        }
+
+        // Authenticate API Session
+        $this->apiSession->loginAs($user);
+
+        $this->status(true);
+        $this->response()->set("email", $user->email);
+        $this->response()->set("username", $user->username);
+    }
+}
---
+++ docs/api/ERRORS.md
@@ -0,0 +1,29 @@
+Error Code | Message | Possible Resolution
+--- | --- | ---
+API_CONTROLLER_DISABLED | This endpoint is currently disabled | n/a
+INTERNAL_ERROR  | An internal error has occurred | n/a
+PLATFORM_ACCESS_ERROR | An error occurred while retrieving API access triggers | n/a
+ALREADY_LOGGED_IN | You are already logged in! | Redirect user to authenticated screen (i.e. dashboard)
+RECAPTCHA_REQ | ReCaptcha validation is required | n/a
+RECAPTCHA_FAILED | ReCaptcha validation was failed | Reset reCaptcha on user screen
+FIRST_NAME_REQ | First name is required | Focus on input field
+FIRST_NAME_LEN | First name must be between 3 and 16 characters | Focus on input field
+FIRST_NAME_INVALID | First name contains an illegal character | Focus on input field
+LAST_NAME_REQ | Last name is required | Focus on input field
+LAST_NAME_LEN | Last name must be between 3 and 16 characters | Focus on input field
+LAST_NAME_INVALID | Last name contains an illegal character | Focus on input field
+EMAIL_ADDR_REQ | E-mail address is required | Focus on input field
+EMAIL_ADDR_INVALID | E-mail address is invalid | Focus on input field
+EMAIL_ADDR_LEN | E-mail address is too long | Focus on input field
+EMAIL_ADDR_DUP | E-mail address is already registered | Focus on input field
+USERNAME_LEN_MIN | Username is too short | Focus on input field
+USERNAME_LEN_MAX | Username is too long | Focus on input field
+USERNAME_INVALID | Username contains an illegal character | Focus in input field
+USERNAME_DUP | Username is already taken | Focus on input field
+PASSWORD_REQ | Password is required | Focus on input field
+PASSWORD_LEN_MIN | Password must be 8 digits or longer | Focus on input field
+PASSWORD_LEN_MAX | Password cannot exceed 32 characters | Focus on input field
+PASSWORD_WEAK | Password is too weak | Focus on input field, Suggest a stronger password
+PASSWORD_CONFIRM_MATCH | You must retype same password | Focus on input field
+COUNTRY_INVALID | Select a country | Focus on input field
+TERMS_UNCHECKED | You must agree with our Terms & Conditions | Focus on checkbox
