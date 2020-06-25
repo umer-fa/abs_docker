fffb661fe839e21490264c8fc57274e7fbc96696
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-06-25 09:36:27 +0500

    undo

--- app/common/Users/Params.php
+++ app/common/Users/Params.php
@@ -3,10 +3,6 @@ declare(strict_types=1);
 
 namespace App\Common\Users;
 
-use Comely\DataTypes\Buffer\Binary;
-use Comely\Utils\Security\Exception\PRNG_Exception;
-use Comely\Utils\Security\PRNG;
-
 /**
  * Class Params
  * @package App\Common\Users
@@ -15,8 +11,6 @@ class Params
 {
     /** @var int */
     public int $user;
-    /** @var string|null */
-    public ?string $emailVerifyBytes = null;
     /** @var string|null */
     public ?string $resetToken = null;
     /** @var int|null */
@@ -29,26 +23,6 @@ class Params
     public function __construct(User $user)
     {
         $this->user = $user->id;
-    }
-
-    /**
-     * @param int $len
-     * @return Binary
-     */
-    public function setEmailVerifyBytes(int $len = 4): Binary
-    {
-        if ($len < 4 || $len > 16) {
-            throw new \RangeException('E-mail verification token must be 4-16 bytes');
-        }
-
-        try {
-            $rand = PRNG::randomBytes($len);
-        } catch (PRNG_Exception $e) {
-            throw new \UnexpectedValueException('Failed to generate random bytes');
-        }
-
-        $this->emailVerifyBytes = $rand->raw();
-        return $rand->readOnly(true);
     }
 
     /**
