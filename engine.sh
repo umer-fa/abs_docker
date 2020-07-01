d5cbfb0fbc2c6e45754595a5a6510ce0f30db342
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-07-01 18:57:10 +0500

    disable TTY

--- engine.sh
+++ engine.sh
@@ -3,5 +3,5 @@ SCRIPT=`realpath $0`
 SCRIPT_PATH=`dirname $SCRIPT`
 
 cd $SCRIPT_PATH/docker
-docker-compose exec engine /home/comely-io/engine/src/console $@
+docker-compose exec -T engine /home/comely-io/engine/src/console $@
 cd ../
