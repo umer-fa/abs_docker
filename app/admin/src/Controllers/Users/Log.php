babe100dff8a62c7390b770a8a4529523d37a7d7
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-07-14 12:24:44 +0500

    log page update

--- app/admin/src/Controllers/Users/Log.php
+++ app/admin/src/Controllers/Users/Log.php
@@ -165,7 +165,7 @@ class Log extends AbstractAdminController
                     $log = new \App\Common\Users\Log($logRow);
                     $logUser = Users::get($log->user);
                     $log = Validator::JSON_Filter($log, sprintf("log:%d", $log->id));
-                    $log["data"] = $log["data"] ? base64_decode($log["data"]) : "[]";
+                    $log["data"] = json_encode($log["data"]);
                     $log["username"] = $logUser->username;
                     $result["rows"][] = $log;
                 } catch (\Exception $e) {
