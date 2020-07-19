0849fea045c1602706a447f97692892d9d3d8f3e
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-07-19 15:12:49 +0500

    htmlspecialchars encode password

--- app/common/Users/UserEmailsPresets.php
+++ app/common/Users/UserEmailsPresets.php
@@ -218,7 +218,7 @@ class UserEmailsPresets
                 "username" => $user->username,
                 "email" => $user->email,
             ],
-            "newPassword" => $newPassword,
+            "newPassword" => htmlspecialchars($newPassword),
         ]);
 
         $mail->addToQueue();
