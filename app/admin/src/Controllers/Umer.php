7b703656171ae21db507da88235323093dcc00f9
umer-fa <hetrosoft@gmail.com>
2021-01-07 12:55:42 +0500

    ass

---
+++ app/admin/src/Controllers/Umer.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Admin\Controllers;
+
+/**
+ * Class Dashboard
+ * @package App\Admin\Controllers
+ */
+class Umer extends AbstractAdminController
+{
+    public function adminCallback(): void
+    {
+    }
+
+    /**
+     * @throws \Comely\Knit\Exception\KnitException
+     * @throws \Comely\Knit\Exception\TemplateException
+     */
+    public function get(): void
+    {
+        echo 'umer farooq chattha';
+    }
+}
